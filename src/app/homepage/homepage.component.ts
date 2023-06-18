46b1803c1cead9a82150a9aa90e22c9ce27fef2c
Misael Alberto <misael.alberto@outlook.com>
2023-06-18 19:42:04 -0300

    Adicionando componente Home

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import Sessao from '../services/sessao';
+import { Router } from '@angular/router';
+import { Veiculo } from 'src/app/models/veiculo'
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+
+export class HomepageComponent implements OnInit {
+  veiculos?: Veiculo[]
+
+  constructor(private router: Router, public http: HttpClient) { }
+
+  ngOnInit(): void {
+    const usuarioId = Sessao.getUsuario();
+    if (!usuarioId) {
+      this.router.navigateByUrl('/login')
+      return
+    }
+    this.loadVeiculos()
+  }
+
+  async loadVeiculos() {
+    this.veiculos = await Veiculo.todos(this.http)
+  }
+}
