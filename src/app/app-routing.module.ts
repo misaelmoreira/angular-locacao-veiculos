3e4fb1f3d0a492cc5ad75a0262c45236c11ada05
Misael Alberto <misael.alberto@outlook.com>
2023-06-18 19:41:41 -0300

    Adicionando rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import {HomepageComponent} from "./homepage/homepage.component"
+import {LoginComponent} from "./login/login.component"
+
+
+const routes: Routes = [
+    {
+        path: '', component: HomepageComponent
+    },
+    {
+        path: 'login', component: LoginComponent 
+    }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
